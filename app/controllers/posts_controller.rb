670e22db37f7e780d047883da8bb1540211e10c7
Maxim <karamba92@mail.ru>
2016-09-22 19:42:49 +0600

    Revert "Revert "Revert "asdf edit post cont"""
    This reverts commit a23226cc55f774e713f738b0a167d60953879d4e.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 	before_action :set_post, only: [:show, :edit, :update, :destroy]
 	before_action :owned_post, only: [:edit, :update, :destroy] 
-asdf
+
 	def index
 		@posts = Post.all
 		@comment = Comment.new
