21acd743164485508c17c89f895501f6b0f0e71f
Maxim <karamba92@mail.ru>
2016-09-23 15:49:23 +0600

    123

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 	before_action :set_post, only: [:show, :edit, :update, :destroy]
 	before_action :owned_post, only: [:edit, :update, :destroy] 
-asdf test4 add to master afdafsasdfdsf
+asdf test4 add to master afdafsasdfdsf  adfasdf
 	def index
 		@posts = Post.all
 		@comment = Comment.new
